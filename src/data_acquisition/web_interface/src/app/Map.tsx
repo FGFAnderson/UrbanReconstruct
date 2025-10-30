1a4f072302498f974e607c987c1dcc494e0b7cb2
fgfanderson <fgfanderson@gmail.com>
2025-10-30 18:25:16 +0000

    feat: Changed to use maplibre for peformance

--- src/data_acquisition/web_interface/src/app/Map.tsx
+++ src/data_acquisition/web_interface/src/app/Map.tsx
@@ -1,73 +1,96 @@
 "use client"
 
-import { MapContainer, TileLayer, useMap } from "react-leaflet"
-import "leaflet/dist/leaflet.css"
-import L, { LatLngExpression } from "leaflet"
-import { useEffect } from "react"
-
-function MapillaryLayer() {
-  const map = useMap()
-  
-  useEffect(() => {
-    import('leaflet.vectorgrid').then(() => {
-      const MAPILLARY_TOKEN = process.env.NEXT_PUBLIC_MAPILLARY_TOKEN
-      
-      const mapillaryLayer = (L as any).vectorGrid.protobuf(
-        `https://tiles.mapillary.com/maps/vtp/mly1_public/2/{z}/{x}/{y}?access_token=${MAPILLARY_TOKEN}`,
-        {
-          vectorTileLayerStyles: {
-            sequence: function(properties: any) {
-              if (!properties.is_pano) {
-                return {
-                  opacity: 0,
-                }
-              }
-              return {
-                radius: 1,
-                weight: 1,
-                color: '#05CB63',
-                fillColor: '#05CB63',
-                fillOpacity: 0.8
-              }
-            },
-            image: {
-                opacity: 0
-            },
-            overview: {
-                opacity: 0
-            },
-          },
-          maxNativeZoom: 14,
-          interactive: true,
-          getFeatureId: function(f: any) {
-            return f.properties.id
-          }
-        }
-      ).addTo(map)
-      
-      mapillaryLayer.on('click', function(e: any) {
-        if (e.layer.properties) {
-          console.log('Clicked:', e.layer.properties)
-        }
-      })
-    })
-  }, [map])
-  
-  return null
-}
+import { useEffect, useRef } from "react"
+import maplibregl from "maplibre-gl"
+import "maplibre-gl/dist/maplibre-gl.css"
 
 interface MapProps {
-  position: LatLngExpression | undefined;
+  position: [number, number];
   zoom: number;
 }
 
-export default function Map({position, zoom}: MapProps) {
-  
-  return <MapContainer center={position} zoom={zoom} scrollWheelZoom={true} style={{ height: "100%", width: "100%" }}>
-    <TileLayer
-      attribution="Esri"
-      url="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
-    />
-    <MapillaryLayer></MapillaryLayer>
-  </MapContainer>
+export default function Map({ position, zoom }: MapProps) {
+  const mapContainer = useRef<HTMLDivElement>(null)
+  const mapRef = useRef<maplibregl.Map>()
+
+  useEffect(() => {
+    if (!mapContainer.current) return
+
+    const map = new maplibregl.Map({
+      container: mapContainer.current,
+      style: {
+        version: 8,
+        sources: {
+          "esri-satellite": {
+            type: "raster",
+            tiles: [
+              "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}"
+            ],
+            tileSize: 256,
+          }
+        },
+        layers: [{
+          id: "esri-satellite",
+          type: "raster",
+          source: "esri-satellite",
+        }]
+      },
+      center: position,
+      zoom: zoom,
+    })
+
+    mapRef.current = map
+
+    map.on("load", () => {
+      const token = process.env.NEXT_PUBLIC_MAPILLARY_TOKEN
+      if (!token) return
+
+      map.addSource("mapillary", {
+        type: "vector",
+        tiles: [`https://tiles.mapillary.com/maps/vtp/mly1_public/2/{z}/{x}/{y}?access_token=${token}`],
+      })
+
+      map.addLayer({
+        id: "panos",
+        type: "circle",
+        source: "mapillary",
+        "source-layer": "sequence",
+        filter: ["==", ["get", "is_pano"], true],
+        paint: {
+          "circle-radius": [
+            "interpolate",
+            ["linear"],
+            ["zoom"],
+            10, 3,
+            16, 6,
+            22, 10
+          ],
+          "circle-color": "#05CB63",
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#ffffff",
+        }
+      })
+      
+      map.on("mouseenter", "panos", () => {
+        map.getCanvas().style.cursor = "pointer"
+      })
+
+      map.on("mouseleave", "panos", () => {
+        map.getCanvas().style.cursor = ""
+      })
+
+      map.on("click", "panos", (e) => {
+        console.log("Clicked:", e.features?.[0].properties)
+      })
+    })
+
+    return () => map.remove()
+  }, [])
+
+  useEffect(() => {
+    mapRef.current?.setCenter(position)
+    mapRef.current?.setZoom(zoom)
+  }, [position, zoom])
+
+  return <div ref={mapContainer} style={{ width: "100%", height: "100%" }} />
 }
