1545ae5bef7d15fe63f9ac9d2b97bc6d8389a5ee
fgfanderson <fgfanderson@gmail.com>
2025-11-18 11:13:03 +0000

    feat: Added function to detect if image is 360

--- src/data_processing/image_preprocessing/main.py
+++ src/data_processing/image_preprocessing/main.py
@@ -1,5 +1,25 @@
+from PIL import Image
+
+def is_360_image(image_path: str) -> bool:
+    """Finds if an image is 360 based on its aspect ratio
+
+    Args:
+        image_path (str): Path to image
+
+    Returns:
+        bool: If the image is 360 return true, if not false
+    """
+    img = Image.open(image_path)
+    width, height = img.size
+    
+    aspect_ratio = width / height
+    if 1.9 < aspect_ratio < 2.1:
+        return True
+    
+    return False
+
 def main():
-    print("Hello from image-preprocessing!")
+    print(is_360_image("./9V1enNuaCEx-O_byo0RVIg_0001_1345970072439378.jpg"))
 
 
 if __name__ == "__main__":
